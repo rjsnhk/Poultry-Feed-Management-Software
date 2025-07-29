d5d7838c723cd7f780c76d93dd7a888fa2f46f35
Subhash Kumar <subhashkushwah134@gmail.com>
2025-07-29 14:53:17 +0530

    hello

--- backend/models/PlantHead.js
+++ backend/models/PlantHead.js
@@ -5,7 +5,6 @@ const plantHeadSchema = new mongoose.Schema({
   password: String,
   phone: String,
   role: { type: String, default: 'PlantHead' },
-  warehouse: { type: mongoose.Schema.Types.ObjectId, ref: 'Warehouse', unique: true }, // One-to-one
 }, { timestamps: true });
 
 module.exports = mongoose.model('PlantHead', plantHeadSchema);
